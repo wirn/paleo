552372ecb3361e12c72e8f41464344be9505300d
Eric <eric.bjorklund@xlent.se>
2024-02-03 16:21:48 +0100

    la till dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ingredientsAllowed } from './data';
+import { Ingredient } from './models/ingredient';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FilterPipe, PipesModule } from './filter.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule, PipesModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'paleo';
+export class AppComponent implements OnInit {
+  ingredienList: Ingredient[] = [];
+  searchForm: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.ingredienList = ingredientsAllowed;
+    this.searchForm = this.fb.group({
+      searchText: '',
+    });
+  }
 }
